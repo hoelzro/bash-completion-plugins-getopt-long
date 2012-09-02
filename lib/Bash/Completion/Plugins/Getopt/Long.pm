84cc1a76b12d72ededf49cf3e8b26e1d3a228899
Rob Hoelz <rob@hoelz.ro>
2012-09-02 23:08:17 +0200

    initial commit

---
+++ lib/Bash/Completion/Plugins/Getopt/Long.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Bash::Completion::Plugins::Getopt::Long;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Bash::Completion::Plugins::Getopt::Long
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
