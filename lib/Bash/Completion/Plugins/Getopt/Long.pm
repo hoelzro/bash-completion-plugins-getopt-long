52f0187b62bd174cb0dfb77d0be129ffe45d0dc7
Rob Hoelz <rob@hoelz.ro>
2012-09-03 08:33:23 +0200

    Add docs

--- lib/Bash/Completion/Plugins/Getopt/Long.pm
+++ lib/Bash/Completion/Plugins/Getopt/Long.pm
@@ -9,14 +9,22 @@ use warnings;
 
 __END__
 
-# ABSTRACT: A short description of Bash::Completion::Plugins::Getopt::Long
+# ABSTRACT: Bash::Completion support for scripts that use Getopt::Long
 
 =head1 SYNOPSIS
 
+ use parent 'Bash::Completion::Plugins::Getopt::Long';
+
+ # XXX details pending
+
 =head1 DESCRIPTION
 
-=head1 FUNCTIONS
+This is an abstract plugin superclass for L<Bash::Completion> plugins that
+wish to support scripts written with L<Getopt::Long> or can have their
+options specified using L<Getopt::Long>-style syntax.
 
 =head1 SEE ALSO
+
+L<Bash::Completion>, L<Getopt::Long>
 
 =cut
